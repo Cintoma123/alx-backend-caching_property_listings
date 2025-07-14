3eaa20899b0cf7399776810e7219fd6f8f508a33
Cintoma123 <calistusumehnnachebe@gmail.com>
2025-07-14 06:59:18 +0100

    updated my properties/views.py by fixings bugs

--- properties/views.py
+++ properties/views.py
@@ -8,12 +8,13 @@ from django.views import View
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import viewsets
+from django.http import JsonResponse
 # Create your views here.
 class PropertyViewSet(viewsets.ViewSet):
     """
     A simple ViewSet for listing or retrieving properties.
     """
-    @method_decorator(cache_page(60 * 15))
+    @cache_page(60 * 15)
     def property_list(self, request):
         contents = {
             "title": request.user.get_title()
@@ -24,6 +25,7 @@ class PropertyViewSet(viewsets.ViewSet):
 
         }
         return Response(contents)
+        return JsonResponse({"request": "data", "contents": contents})
       # Cache the response for 15 minutes
     
     
